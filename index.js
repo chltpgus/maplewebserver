e551a406193559c81ffdc07b0e2bb0154eb69fc3
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-19 10:35:22 +0900

    2

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+ 
+var o = {} // empty Object
+var key = 'reponse';
+o[key] = []; // empty Array, which you can push() values into
+ 
+ 
+var data = {
+    name: 'jason',
+    age: '33'
+};
+var data2 = {
+    name: 'mike',
+    data: '34'
+};
+o[key].push(data);
+o[key].push(data2);
+ 
+var result = JSON.stringify(o);
+ 
+app.get("/", (req, res) => {
+res.send(result);
+});
+app.listen(port);
+
+
+출처: https://jason24k.tistory.com/9 [제이슨's 블로그:)]
