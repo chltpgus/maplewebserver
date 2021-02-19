8b84e1191fb030034de45ee3ce783d5a354eac86
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-19 21:30:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
- 
-var o = {} // empty Object
-var key = 'reponse';
-o[key] = []; // empty Array, which you can push() values into
- 
- 
-var data = {
-    name: 'jason',
-    age: '33'
-};
-var data2 = {
-    name: 'mike',
-    data: '34'
-};
+const PORT = process.env.PORT
 
-
-o[key].push(data);
-o[key].push(data2);
- 
-var result = JSON.stringify(o);
- 
-app.get("/api/user", (req, res) => {
-res.send(result);
+app.get("/", (req, res) => {
+res.send({ hello: "world" });
 });
-app.listen(port);
+
+app.listen(PORT);
