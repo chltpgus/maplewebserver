69beec4f733f7c311b8830072880f0eb0b2c3d0f
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-19 21:59:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,19 @@ server.get("/api/user", (req, res) => {
 res.json(users);
 });
 
+server.get("/api/user/:id", (req, res) => {
+    const user = users.find((u)=>{
+        return u.id === req.params.id;
+    });
+    if(user){
+        res.json(user);
+    } else{
+        res.status(404).json({ errorMessage : "User was not found"});
+    }
+});
+
+server.post("/api/user", (req, res) => {
+    res.json(users);
+    });
+
 server.listen(PORT);
