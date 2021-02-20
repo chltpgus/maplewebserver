760891b6a9f99200b156af25daa87b80626adef1
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-20 18:10:26 +0900

    34

--- index.js
+++ index.js
@@ -3,67 +3,61 @@ const server = express();
 const PORT = process.env.PORT
 
 const sqlite3 = require('sqlite3').verbose();
-let id = 1, name = "user", num = 1;
+let id = 1, name = "user number", num = 3;
 
 let UPDATE = 'UPDATE user SET num = '+ num + ' WHERE id =' + id;
+
+
 
 let db = new sqlite3.Database('sqlite.db',(err)=>{
     if(err){
         console.log(err.message);
-    }
-    
+    }  
     console.log('connected to db'); 
-      
+});
+
+db.each("SELECT * FROM user", function(err, row){
+
     let users = 
     [{
         id : 1,
         name : "user",
         num : 1
     }];
+    users = row;
 
-
-    db.run(UPDATE, function(err){
-        if(err){
-            return console.log(err.message);
-        }
+    server.get("/api/user", (req, res) => {
+        res.json(users);
+        });
         
-        console.log('UPDATE');
-    });
+        server.get("/api/user/:id", (req, res) => {
+            const user = users.find((u)=>{
+                return u.id === req.params.id;
+            });
+            if(user){
+                res.json(user);
+            } else{
+                res.status(404).json({ errorMessage : "User was not found"});
+            }
 
-
-
-    db.each("SELECT * FROM user", function(err, row){
-        users = row;
-    
-        server.get("/api/user", (req, res) => {
+        });
+        
+        server.post("/api/user", (req, res) => {
             res.json(users);
             });
-            
-            server.get("/api/user/:id", (req, res) => {
-                const user = users.find((u)=>{
-                    return u.id === req.params.id;
-                });
-                if(user){
-                    res.json(user);
-                } else{
-                    res.status(404).json({ errorMessage : "User was not found"});
-                }
-
-            });
-            
-            server.post("/api/user", (req, res) => {
-                res.json(users);
-                });
-            
-            server.listen(PORT);
-            
-
-    })
-
-    
+        
+        server.listen(PORT);
+        
 
 });
 
+db.run(UPDATE, function(err){
+    if(err){
+        return console.log(err.message);
+    }
+    console.log(UPDATE);
+    console.log('UPDATE');
+});
 
 db.close();
 
