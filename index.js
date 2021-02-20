b326794277c7b7932936c2821843b039d698adca
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-20 21:05:46 +0900

    sdf

--- index.js
+++ index.js
@@ -18,21 +18,49 @@ let db = new sqlite3.Database('sqlite.db',(err)=>{
     console.log('connected to db'); 
 });
 
-db.run(UPDATE, function(err){
-    if(err){
-        return console.log(err.message);
-    }
 
-    console.log(UPDATE);
-    console.log('UPDATE');
-});
+server.get("/api/user", (req, res) => {
+
+
+    let db = new sqlite3.Database('sqlite.db',(err)=>{
+        if(err){
+            console.log(err.message);
+        }  
+        console.log('connected to db'); 
+    });
+
+    let users = 
+    [{
+        id : 1,
+        name : "user number",
+        num : 1
+    }];
+
+    console.log(req.body.num);
+
+    users = req.body;
+
+    num = req.body.num;
+
+    let UPDATE = 'UPDATE user SET num = '+ num + ' WHERE id =' + id;
+
+    db.run(UPDATE, function(err){
+        if(err){
+            return console.log(err.message);
+        }
+    
+        console.log(UPDATE);
+        console.log('UPDATE');
+    });
+
+    });
 
 db.each("SELECT * FROM user", function(err, row){
 
     let users = 
     [{
         id : 1,
-        name : "user",
+        name : "user number",
         num : 1
     }];
     users = row;
@@ -41,23 +69,11 @@ db.each("SELECT * FROM user", function(err, row){
         res.json(users);
         });
         
-        server.post("/api/user", (req, res) => {
-            res.json(users);
-            console.log(req.body);
-
-        
-            db.run(UPDATE, function(err){
-                if(err){
-                    return console.log(err.message);
-                }
-            
-                console.log(UPDATE);
-                console.log('UPDATE');
-            });
-
-            });
-        
         server.listen(PORT);
+        /*
+        server.listen(3000, () => {
+            console.log(`3000번 port에 http server를 띄웠습니다.`)
+          });*/
         
 
 });
