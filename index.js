8bd90ef671fb482483cadb00ae8e7f1cb77e4d02
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-20 16:20:55 +0900

    3

--- index.js
+++ index.js
@@ -2,12 +2,75 @@ const express = require("express");
 const server = express();
 const PORT = process.env.PORT
 
-let users = 
-    {
-        id : "1",
+const sqlite3 = require('sqlite3').verbose();
+let id = 1, name = "user number", num = 1;
+
+let UPDATE = 'UPDATE user SET num = '+ num + ' WHERE id =' + id;
+
+let db = new sqlite3.Database('sqlite.db',(err)=>{
+    if(err){
+        console.log(err.message);
+    }
+    console.log('connected to db'); 
+      
+    let users = 
+    [{
+        id : 1,
         name : "user",
-        num : "1"
-    };
+        num : 1
+    }];
+
+    db.each("SELECT * FROM user", function(err, row){
+        users = row;
+      
+        server.get("/api/user", (req, res) => {
+            res.json(users);
+            });
+            
+            server.get("/api/user/:id", (req, res) => {
+                const user = users.find((u)=>{
+                    return u.id === req.params.id;
+                });
+                if(user){
+                    res.json(user);
+                } else{
+                    res.status(404).json({ errorMessage : "User was not found"});
+                }
+            });
+            
+            server.post("/api/user", (req, res) => {
+                res.json(users);
+                });
+            
+            server.listen(PORT);
+
+    })
+
+    
+
+});
+
+//console.log(UPDATE);
+
+db.run(UPDATE, function(err){
+    if(err){
+        return console.log(err.message);
+    }
+    console.log('UPDATE');
+});
+
+db.close();
+
+
+/*
+let users = 
+    [{
+        id : 1,
+        name : "user",
+        num : 1
+    }
+];
+*/
 
 
 server.all('/*', function(req, res, next) {
@@ -16,7 +79,8 @@ server.all('/*', function(req, res, next) {
     next();
   });
    
-  
+ 
+/*
 server.get("/api/user", (req, res) => {
 res.json(users);
 });
@@ -37,3 +101,4 @@ server.post("/api/user", (req, res) => {
     });
 
 server.listen(PORT);
+*/
