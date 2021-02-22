653f1a9f5d9e266457107d5e975bc5cf1bebebf2
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-22 22:54:52 +0900

    34

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ server.use(bodyParser.json());
 const sqlite3 = require('sqlite3').verbose();
 let id = 1, name = "user number", num = 3;
 
+let users = 
+    [{
+        id : 1,
+        name : "user number",
+        num : 1
+    }];
+
+
+
+global.users = users;
+
+
 let UPDATE = 'UPDATE user SET num = '+ num + ' WHERE id =' + id;
 
 
@@ -24,37 +36,36 @@ db.run(UPDATE, function(err){
         return console.log(err.message);
     }
 
-    console.log(UPDATE);
     console.log('UPDATE');
 });
 
-db.each("SELECT * FROM user", function(err, row){
+ db.each("SELECT * FROM user", function(err, row){
 
-    let users = 
-    [{
-        id : 1,
-        name : "user number",
-        num : 1
-    }];
-    users = row;
+
+});
+
+
+server.get("/api/users", (req, res) => {
+    res.json(req);
+    console.log(req.body);
 
 });
 
 server.get("/api/user", (req, res) => {
-    res.json(req);
-    console.log(req.body);   
 
+    db.each("SELECT * FROM user", function (err, row) {
+        res.json(row);
+        console.log('SELECT');
     });
 
-    server.get("/api/user", (req, res) => {
-        res.json(users);
-        });
-        
-        server.listen(PORT);
-        /*
-        server.listen(3000, () => {
-            console.log(`3000번 port에 http server를 띄웠습니다.`)
-          });*/
+});
+
+server.listen(PORT);
+
+/*
+server.listen(3000, () => {
+    console.log(`3000번 port에 http server를 띄웠습니다.`)
+  });*/
 
 
 
