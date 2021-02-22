9ed0a51a5b8e49ab9a33c006bf3bce256e1cc6c7
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-22 23:42:19 +0900

    34

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ let users =
 
 
 
-global.users = users;
-
-
-let UPDATE = 'UPDATE user SET num = ' + num + ' WHERE id =' + id;
+// let UPDATE = 'UPDATE user SET num = ' + num + ' WHERE id =' + id;
 
 
 
@@ -30,7 +27,7 @@ let db = new sqlite3.Database('sqlite.db', (err) => {
     console.log('connected to db');
 });
 
-
+/*
 db.run(UPDATE, function (err) {
     if (err) {
         return console.log(err.message);
@@ -38,42 +35,56 @@ db.run(UPDATE, function (err) {
 
     console.log('UPDATE');
 });
-
+*/
 
 server.get("/api/user", (req, res) => {
-    
-    let db = new sqlite3.Database('sqlite.db', (err) => {
+
+    let db = new sqlite3.Database('sqlite.db', (err) => { // 디비 불러오기
         if (err) {
             console.log(err.message);
         }
         console.log('connected to db');
     });
 
-    db.each("SELECT * FROM user", function (err, row) {
-        res.json(row);
+    db.each("SELECT * FROM user", function (err, row) { // 디비에서 user정보 가져오기
+        res.json(row);                                  // 서버에 json으로 보내기
     });
 });
-/*
-db.each("SELECT * FROM user", function (err, row) {
-    console.log(row);
-});
-*/
+
 
 server.get("/api/users", (req, res) => {
-    res.json(req);
-    console.log(req.body);
+
+    users = req.body;
+    console.log(users);
+    num = users.num, id= users.id;
+    let UPDATE = 'UPDATE user SET num = ' + num + ' WHERE id =' + id;
+
+    console.log(UPDATE);
+    
+    let db = new sqlite3.Database('sqlite.db', (err) => { // 디비 불러오기
+        if (err) {
+            console.log(err.message);
+        }
+        console.log('connected to db');
+    });
+
+    db.run(UPDATE, function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        console.log('UPDATE');
+    });
 
 });
 
 
 
-server.listen(PORT);
+//server.listen(PORT);
 
-/*
+
 server.listen(3000, () => {
     console.log(`3000번 port에 http server를 띄웠습니다.`)
-  });*/
-
+  });
 
 
 
@@ -82,7 +93,8 @@ db.close();
 
 
 server.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://mapleaing.netlify.app");
+    //res.header("Access-Control-Allow-Origin", "https://mapleaing.netlify.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
