1c7f7ec55049eb4b9868cb306dc96ae113df2995
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-02-25 01:01:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
-
-const express = require("express");
-const bodyParser = require('body-parser')
+const express = require("express");  //express 서버 구성
+const bodyParser = require('body-parser') // 바디펄스로 POST로 온 것을 확인
 const server = express();
 const http = require("http");
-const PORT = process.env.PORT
+const PORT = process.env.PORT  //포트번호 저장
 server.use(bodyParser.json());
-let id = 1, name = "user number", num = 3;
-const cors = require('cors');
-const corsOptions = {
-  origin: "https://mapleaing.netlify.app",
-  credentials: true
-}
-server.use(cors(corsOptions));
+const cors = require('cors');   
 
-let mysql      = require('mysql');
-
-
-setInterval(() => {
-    http.get("http://mapleing.herokuapp.com/api/user");
-}, 3000);
-
-let connection = mysql.createConnection({
-    host     : 'us-cdbr-east-03.cleardb.com',
-    port     : '3306',
-    user     : 'b932d2fb62d529',
-    password : 'b5b268a8',
-    database : 'heroku_a3e154cc90440ef'
-   });
-/*
-   connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected to mysql!");
-  });
-
-  */
-
-let users =
+let id = 1, name = "user number", num = 3;  //MYSQL UPDATE에 들어가는 변수들 선언
+let users =   //사용자 수가 들어가는 객체 선언
     [{
         id: 1,
         name: "user number",
         num: 1
     }];
 
+const corsOptions = {  //클라이언트가 cors문제 없이 들어 올 수 있게 권한을 줌
+  origin: "https://mapleaing.netlify.app",
+  credentials: true
+}
+server.use(cors(corsOptions));
 
-server.get("/api/user", (req, res) => {
+let mysql = require('mysql');  //ClearDB MYSQL 사용
+
+setInterval(() => {   //MYSQL이 방치되면 꺼지는 것을 막기위해 주소를 계속 보내줌
+    http.get("http://mapleing.herokuapp.com/api/user");
+}, 3000);
+
+let connection = mysql.createConnection({    //MYSQL CONNECTION
+    host     : 'us-cdbr-east-03.cleardb.com',
+    port     : '3306',
+    user     : 'b932d2fb62d529',
+    password : 'b5b268a8',
+    database : 'heroku_a3e154cc90440ef'
+   });
+
+
+
+server.get("/api/user", (req, res) => { // get요청이 오면 
 
     connection.query("SELECT * FROM user", function (err, row) {
 
@@ -54,7 +47,7 @@ server.get("/api/user", (req, res) => {
         num = users.num, id= users.id;
         let UPDATE = "UPDATE user SET num = '" + num + "'"+ " WHERE id ='" + id+"'";
     
-        connection.query(UPDATE, function (err) {
+        connection.query(UPDATE, function (err) {  //클라이언트한테 온 정보를 업데이트
             if (err) {
                 return console.log(err.message);
             }
@@ -66,7 +59,7 @@ server.get("/api/user", (req, res) => {
 
 });
 
-server.post("/api/user", (req, res) => {
+server.post("/api/user", (req, res) => { // post 요청이 오면
 
     connection.query("SELECT * FROM user", function (err, row) {
 
@@ -77,7 +70,7 @@ server.post("/api/user", (req, res) => {
         num = users.num, id= users.id;
         let UPDATE = "UPDATE user SET num = '" + num + "'"+ " WHERE id ='" + id+"'";
     
-        connection.query(UPDATE, function (err) {
+        connection.query(UPDATE, function (err) {  //클라이언트한테 온 정보를 업데이트
             if (err) {
                 return console.log(err.message);
             }
@@ -88,32 +81,8 @@ server.post("/api/user", (req, res) => {
     });
 
 });
-
-
-
-
-
 
 server.listen(PORT);
-
-/*
-server.listen(3000, () => {
-    console.log(`3000번 port에 http server를 띄웠습니다.`)
-  });
-
-
-*/
-
-/*
-server.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://mapleaing.netlify.app");
-    //res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-*/
-
 
 /*
 
